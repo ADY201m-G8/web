45c32a70b4e57a8388c43c9409125f71791f9ec3
ForeverWeLearn <theanh11052006@gmail.com>
2026-06-19 11:09:48 +0700

    feat: add svelte-shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { env } from '$env/dynamic/public';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { onMount } from 'svelte';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { Refresh, Remove } from '@steeze-ui/material-design-icons';
 
 	type AttendanceItem = {
 		id: string;
@@ -21,7 +24,7 @@
 	};
 </script>
 
-<div class="flex flex-wrap gap-12 p-12">
+<div class="flex flex-wrap justify-center gap-12 p-12 *:basis-2/12">
 	{#each attendance as att (att.id)}
 		<div class={['border', att.present == 1 ? 'bg-green-200' : '']}>
 			<p>{att.id}</p>
@@ -30,4 +33,14 @@
 	{/each}
 </div>
 
-<button class="m-12 border p-4" onclick={fetchAttendance}>Fetch</button>
+<div class="flex justify-center gap-4">
+	<Button variant="outline" size="lg" onclick={fetchAttendance}>
+		<Icon src={Refresh} />
+		Refresh
+	</Button>
+
+	<Button variant="destructive" size="lg" onclick={fetchAttendance}>
+		<Icon src={Remove} />
+		Uncheck All
+	</Button>
+</div>
